7a88ec87b4de85a010d1996b1596fd98fbc45070
Jay Deverett <jayjdeverett@gmail.com>
2017-07-10 14:27:02 -0400

    add successful test of contact.create

--- crm.rb
+++ crm.rb
@@ -49,7 +49,12 @@ class CRM
     print 'Enter a Note: '
     note = gets.chomp.downcase
 
-    Contact.create(first_name, last_name, email, note)
+    contact = Contact.create(
+      first_name: first_name,
+      last_name:  last_name,
+      email:      email,
+      note:       note
+    )
   end
 
   def modify_existing_contact
